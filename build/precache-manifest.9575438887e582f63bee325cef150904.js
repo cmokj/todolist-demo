a2f366bf9f2d44b9950204b1bd8af18f25172179
chenminjia <314081655@qq.com>
2019-07-30 09:15:22 +0800

    简单的 TodoList

---
+++ build/precache-manifest.9575438887e582f63bee325cef150904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e563058a7f7eb9ebc024ae1f3f8d4d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bcb7dfd392882559f07e",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0dbbe09614ecd926f1a7",
+    "url": "./static/css/main.8b787fb6.chunk.css"
+  },
+  {
+    "revision": "bcb7dfd392882559f07e",
+    "url": "./static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "0dbbe09614ecd926f1a7",
+    "url": "./static/js/main.4d12f7f4.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
