3b0cef311a962db149720191594f14cfa60384dd
chenminjia <314081655@qq.com>
2019-07-30 09:25:48 +0800

    简单的 TodoList

--- build/precache-manifest.5bf24f5d681422bbad6005f6028cf02d.js
+++ build/precache-manifest.5bf24f5d681422bbad6005f6028cf02d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e7e563058a7f7eb9ebc024ae1f3f8d4d",
+    "revision": "35114c8c72ff3614d3ec993b289a6c7b",
     "url": "./index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.764ccc25.chunk.css"
   },
   {
-    "revision": "0dbbe09614ecd926f1a7",
-    "url": "./static/css/main.8b787fb6.chunk.css"
+    "revision": "04e3e05a613c1cfcef71",
+    "url": "./static/css/main.e93869fa.chunk.css"
   },
   {
     "revision": "bcb7dfd392882559f07e",
     "url": "./static/js/2.2d717d87.chunk.js"
   },
   {
-    "revision": "0dbbe09614ecd926f1a7",
-    "url": "./static/js/main.4d12f7f4.chunk.js"
+    "revision": "04e3e05a613c1cfcef71",
+    "url": "./static/js/main.c09e4d85.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
